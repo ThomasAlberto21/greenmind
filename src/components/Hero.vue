115ca99a4e6e8c0b6377bb170a36d578e3e88494
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-22 11:20:16 +0700

    .

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,30 +1,38 @@
 <template>
-  <section class="hero container mt-3">
-    <div
-      class="w-100 h-100 img-fluid rounded-4"
-      style="background-color: #c1dcdc"
-    >
+  <section class="hero container mt-3 mb-5">
+    <div class="w-100 h-100 rounded-4" style="background-color: #c1dcdc">
       <div class="row">
-        <div class="col px-5 py-4">
-          <h1 class="fw-bolder display-2">
+        <div class="col-lg-6 col-md-12 px-md-5 px-5 py-5 text-start">
+          <h1 class="fw-bolder display-2 text-dark">
             Buy your <br />
             dream plants
           </h1>
-          <div class="d-flex mt-5 gap-5">
-            <p class="h4 fw-normal">
+          <div class="d-flex mt-5 gap-md-5 gap-4">
+            <p class="h5 fw-normal">
               50 + <br />
               Plants Species
             </p>
-            <div class="border border-dark"></div>
-            <p class="h4 fw-normal">
+            <div class="border border-dark opacity-50"></div>
+            <p class="h5 fw-normal">
               100 + <br />
               Customers
             </p>
           </div>
 
-          <button class="btn btn-dark rounded-pill py-3 px-4 mt-5 fw-bold">
+          <router-link
+            to="/products"
+            class="btn btn-dark rounded-pill py-3 px-4 mt-5 fw-bold"
+          >
             See All Product <i class="bi bi-hand-index-thumb ms-2 h5"></i>
-          </button>
+          </router-link>
+        </div>
+
+        <div class="col-lg-6 col-md-12">
+          <img
+            src="../assets/hero.png"
+            alt="hero_img"
+            class="img-fluid w-100 h-100"
+          />
         </div>
       </div>
     </div>
