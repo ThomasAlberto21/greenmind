ab49a5451130c4e59f2e2c048200cf66929fe13f
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-22 11:27:48 +0700

    .

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="hero container mt-3 mb-5">
-    <div class="w-100 h-100 rounded-4" style="background-color: #c1dcdc">
+    <div class="rounded-4" style="background-color: #c1dcdc">
       <div class="row">
-        <div class="col-lg-6 col-md-12 px-5 py-5 text-start">
+        <div class="col-lg-6 col-md-12 px-5 py-5">
           <h1 class="fw-bolder display-2 text-dark">
             Buy your <br />
             dream plants
           </h1>
-          <div class="d-flex mt-5 gap-md-5 gap-4">
-            <p class="h5 fw-normal">
+          <div class="d-flex mt-5 gap-5">
+            <p class="h4 fw-normal">
               50 + <br />
               Plants Species
             </p>
             <div class="border border-dark opacity-50"></div>
-            <p class="h5 fw-normal">
+            <p class="h4 fw-normal">
               100 + <br />
               Customers
             </p>
@@ -21,9 +21,9 @@
 
           <router-link
             to="/products"
-            class="btn btn-dark rounded-pill py-3 px-4 mt-5 fw-bold"
+            class="btn btn-dark rounded-pill px-4 py-3 mt-5 fw-bold"
           >
-            See All Product <i class="bi bi-hand-index-thumb ms-2 h5"></i>
+            See All Product <i class="bi bi-hand-index-thumb h5 ms-2"></i>
           </router-link>
         </div>
 
