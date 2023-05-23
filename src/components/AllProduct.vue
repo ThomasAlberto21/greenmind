6b8371af7498fb4088029ac6e9fa046848055452
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-23 19:24:51 +0700

    .

--- src/components/AllProduct.vue
+++ src/components/AllProduct.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+  <section class="all_product">
+    <router-link to="/products" class="card border-0">
+      <img
+        :src="all_product.image"
+        alt="trending_product_img"
+        style="height: 25rem"
+        class="img-fluid rounded-4"
+      />
+    </router-link>
+    <div class="mt-3 text-dark">
+      <h5 class="fw-bold">
+        {{ all_product.title }}
+      </h5>
+      <p class="fw-normal">${{ all_product.price }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'AllProductComponent',
+  props: ['all_product'],
 };
 </script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,8 +1,18 @@
 <template>
   <section class="products">
     <Navbar />
-    <div class="container">
-      <h1>All Products</h1>
+    <div class="container my-5">
+      <h3 class="fw-normal mb-5">All <strong>Products</strong></h3>
+
+      <div class="row">
+        <div
+          class="col-lg-3 col-md-6 mb-5"
+          v-for="all_product in all_products"
+          :key="all_product.id"
+        >
+          <AllProduct :all_product="all_product" />
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -10,11 +20,32 @@
 <script>
 import axios from 'axios';
 import Navbar from '@/components/Navbar.vue';
+import AllProduct from '@/components/AllProduct.vue';
 
 export default {
   name: 'ProductsView',
   components: {
     Navbar,
+    AllProduct,
+  },
+
+  data() {
+    return {
+      all_products: [],
+    };
+  },
+
+  methods: {
+    setAllProducts(data) {
+      this.all_products = data;
+    },
+  },
+
+  mounted() {
+    axios
+      .get('http://localhost:3000/all_products')
+      .then((response) => this.setAllProducts(response.data))
+      .catch((error) => console.log(error));
   },
 };
 </script>
