5445ccff7c970ea239f2f9863dfc74e0fa312620
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-24 20:58:56 +0700

    .

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    <h1>Cart</h1>
-  </div>
+  <section class="cart">
+    <Navbar />
+    <div class="container my-5">
+      <h1 class="fw-bold mb-5">Cart</h1>
+      <div class="row">
+        <div class="col">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">Image</th>
+                <th scope="col">Name Plants</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue';
+
 export default {
   name: 'CartView',
+  components: {
+    Navbar,
+  },
 };
 </script>
